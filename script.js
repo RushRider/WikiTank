34011609b7b42fed22d5031149ea376d9404ccf2
「Rush」 <tcautenet@myges.fr>
2024-04-10 14:36:45 +0200

    bbb

--- script.js
+++ script.js
@@ -17,6 +17,28 @@ const tank=[
     }
 ]
 
-FileItem(item,key,value)[
+function filtrerListe(liste, key, value) {
+    var result = [];
+    for (var i = 0; i < liste.length; i++) {
+      if (liste[i][key] === value) {
+        resultat.push(liste[i]);
+      }
+    }
+    return result;
+  }
 
-]
+  function orderListe(liste, key, value) {
+    var result = [];
+    var non = [];
+    for (var i = 0; i < liste.length; i++) {
+      if (liste[i][key] === value) {
+        result.push(liste[i]);
+      }else{
+        non.push(liste[i]);
+      }
+    }
+    for (var i = 0; i < non.length; i++) {
+        result.push(non[i]);
+      }
+    return result;
+  }
