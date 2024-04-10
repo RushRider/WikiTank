fb2ddce63b63da4ffea4c7c48e6ac3dd6a11c873
「Rush」 <tcautenet@myges.fr>
2024-04-10 13:01:01 +0200

    aaa

---
+++ script.js
@@ -0,0 +1,22 @@
+const tank=[
+    {
+        name: "ELC bis",
+        poid: "8.5",
+        canon: "90mm",
+        chevaux: "180"
+    },{
+        name: "AMX-13",
+        poid: "14.8",
+        canon: "75mm",
+        chevaux: "250"
+    },{
+        name: "LORRAINE 40T",
+        poid: "39.7",
+        canon: "100mm",
+        chevaux: "850"
+    }
+]
+
+FileItem(item,key,value)[
+
+]
