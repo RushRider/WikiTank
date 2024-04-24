70668c74f77a26f527fcf7da77787cdcb50cfdc7
Pierre <gaufdev@gmail.com>
2024-04-24 16:42:17 +0200

    liste char tous les pays

---
+++ us.js
@@ -0,0 +1,27 @@
+// var url = 'https://api-console.worldoftanks.com/wotx/encyclopedia/vehicles/?language=fr&nation=france';
+var url = 'amerique.json';
+
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('La requête a échoué');
+    }
+    return response.json();
+  })
+  .then(jsonData => {
+    var test = document.getElementById("test");
+    for(let item of jsonData ) {
+      console.log(item);
+      var html = `
+        <div class="content">
+          <h2>${item.nom}</h2>
+          <p>${item.nom}</p>
+        </div>
+      `;
+      test.innerHTML += html;
+  }
+  })
+  .catch(error => {
+    console.error('Erreur :', error);
+  });
+
